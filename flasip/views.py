c0f6d958e09801e3f5a1f79e906722d265bce6b2
thobalose <lose.thoba@gmail.com>
2018-04-14 00:44:42 +0200

    Added Google App Engine configs.

--- flasip/views.py
+++ flasip/views.py
@@ -1,10 +1,13 @@
 from flask import render_template
 from app import app
 import socket
+import os
 
 
 @app.route('/')
 def index():
-    hostname = socket.gethostname()
-    host_ip = socket.gethostbyname(hostname)
+    hostname = os.getenv(
+        'DEFAULT_VERSION_HOSTNAME') or socket.gethostname()
+    host_ip = os.getenv('SERVER_SOFTWARE',
+                        '') or socket.gethostbyname(hostname)
     return render_template("index.html", hostname=hostname, host_ip=host_ip)
