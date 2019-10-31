01bb1c60197c97fff3c583828c06ab6d1195fa64
thoba <lose.thoba@gmail.com>
2019-10-31 18:11:44 +0200

    Exception handling

--- flasip/views.py
+++ flasip/views.py
@@ -5,8 +5,17 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
+def get_hostname():
+    hostname = socket.gethostname()
+    try:
+        ip = socket.gethostbyname(hostname)
+    except socket.gaierror:
+        ip = "127.0.0.1"
+    return hostname, ip
+
+
 @app.route('/')
 def index():
-    hostname = socket.gethostname()
-    host_ip = socket.gethostbyname(hostname)
-    return render_template("index.html", hostname=hostname, host_ip=host_ip)
+    hostname = get_hostname()[0]
+    ip = get_hostname()[1]
+    return render_template("index.html", hostname=hostname, host_ip=ip)
